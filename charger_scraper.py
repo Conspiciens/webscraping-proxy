0eb09bded2afa6b2c5a5fa327f3b76909633a5c2
Conspiciens <fernandezjohann08@gmail.com>
2026-03-17 10:37:25 -0700

    Increase stand off

--- charger_scraper.py
+++ charger_scraper.py
@@ -16,7 +16,7 @@ from collections import deque
 from dataclasses_types import Car
 
 async def request_link(assesion, link: str) -> Optional[requests.Response]: 
-    stand_off = 2
+    stand_off = 3
     tries = 0 
     response = None
 
