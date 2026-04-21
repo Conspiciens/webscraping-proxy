ccc0d93913c0111d123b69a0acae60ebed6a2ad9
Conspiciens <fernandezjohann08@gmail.com>
2026-04-21 22:35:04 +0700

    remove connection connected

--- charger_scraper.py
+++ charger_scraper.py
@@ -141,7 +141,6 @@ async def main():
     asession = AsyncHTMLSession() 
     asession.headers.update({
         "User-Agent" : '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36''',
-        "Connection": "keep-alive",
     })
 
     JSON_FILENAME = "ev.json"
